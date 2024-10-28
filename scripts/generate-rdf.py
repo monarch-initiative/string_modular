dedb0a910ff0d468d1d1885be2f46ff2e20be99f
Justin Reese <justaddcoffee@gmail.com>
2024-10-28 14:36:48 -0400

    Fix (?) generate-rdf.py for this ingest

--- scripts/generate-rdf.py
+++ scripts/generate-rdf.py
@@ -6,8 +6,8 @@ from loguru import logger
 logger.info(f"Creating rdf output: output/string_protein-protein interaction.nt.gz ...")
 
 src_files = []
-src_nodes = f"output/string_protein-protein interaction_nodes.tsv"
-src_edges = f"output/string_protein-protein interaction_edges.tsv"
+src_nodes = f"output/string_modular_nodes.tsv"
+src_edges = f"output/string_modular_edges.tsv"
 
 if Path(src_nodes).is_file():
     src_files.append(src_nodes)
